f8cf47996cccdcc835614d4eefc7e0642b4c2653
Tiffany You <tiffyou@iu.edu>
2025-01-31 14:28:07 -0500

    added comments on FourPointFive

--- FourPointFivePercent.java
+++ FourPointFivePercent.java
@@ -1,6 +1,5 @@
 public class FourPointFivePercent implements SalesTaxBehavior {
-
-
+//    Ovveride so it compute 4.5% tax of given amount
     @Override
     public double compute(double value) {
         return value * 0.045;
