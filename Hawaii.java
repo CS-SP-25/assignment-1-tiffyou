ea0ac69f11e8792bec6b2c0e28a06c6a865f0b96
Tiffany You <tiffyou@iu.edu>
2025-01-31 14:17:58 -0500

    added comments in Hawaii

--- Hawaii.java
+++ Hawaii.java
@@ -1,5 +1,7 @@
 public class Hawaii extends State{
+//    constructor for new state
     public Hawaii(){
+//        super for state constructor and making sure it defaults to 4.5% tax
         super("Hawaii", new FourPointFivePercent());
     }
 }
